62af18c3c036fd62f64a403965f93cd5de69b1c5
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2025-05-08 11:59:36 +0530

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+
+
+// enableProdMode();
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch((err) => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+
+const originalReadAsArrayBufferMethod = FileReader.prototype.readAsArrayBuffer;
+
+FileReader.prototype.readAsArrayBuffer = function (args) {
+  const originalFileReader = this['__zone_symbol__originalInstance'];
+  if (originalFileReader) {
+    originalFileReader.onloadend = () => {
+      this.result = originalFileReader.result;
+      this.onload();
+    }
+    originalFileReader.readAsArrayBuffer(args);
+  } else {
+    originalReadAsArrayBufferMethod(args);
+  }
+}
